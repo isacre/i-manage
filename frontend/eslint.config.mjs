11eb3e8c17228bb34e3040c479213b0ad63622d3
Isaac Melo <isaacalvesx7@gmail.com>
2025-08-26 21:04:36 -0300

    🔧 add storybook setup

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,24 +12,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "no-unused-vars": "warn",
-      "unused-imports/no-unused-imports": "warn",
-      "import/order": [
-        "warn",
-        {
-          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-          alphabetize: { order: "asc", caseInsensitive: true },
-        },
-      ],
-      "import/no-unresolved": "error",
-      "import/no-duplicates": "warn",
-    },
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript"), {
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "warn",
+    "unused-imports/no-unused-imports": "warn",
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "import/no-unresolved": "error",
+    "import/no-duplicates": "warn",
   },
-];
+}, ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
